0373eac6eb2f5490162a3aa3f43642136c21eda2
Ben Chatelain <ben@octop.ad>
2015-05-14 18:50:28 -0600

    Add framework target

---
+++ WatchKitCoreData Framework/WatchKitCoreData Framework.h
@@ -0,0 +1,19 @@
+//
+//  WatchKitCoreData Framework.h
+//  WatchKitCoreData Framework
+//
+//  Created by Ben Chatelain on 5/14/15.
+//  Copyright (c) 2015 Ben Chatelain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WatchKitCoreData Framework.
+FOUNDATION_EXPORT double WatchKitCoreData_FrameworkVersionNumber;
+
+//! Project version string for WatchKitCoreData Framework.
+FOUNDATION_EXPORT const unsigned char WatchKitCoreData_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WatchKitCoreData_Framework/PublicHeader.h>
+
+
