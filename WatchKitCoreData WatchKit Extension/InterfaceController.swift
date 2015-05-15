17d86cc519d5a48d146750bfa6c90a71c2726bd1
Ben Chatelain <ben@octop.ad>
2015-05-14 18:48:07 -0600

    Add watch app targets

---
+++ WatchKitCoreData WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchKitCoreData WatchKit Extension
+//
+//  Created by Ben Chatelain on 5/14/15.
+//  Copyright (c) 2015 Ben Chatelain. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
