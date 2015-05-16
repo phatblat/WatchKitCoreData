05e7ccbe1a5324638448a8aee44340dc1d2a20db
Ben Chatelain <ben@octop.ad>
2015-05-15 20:36:57 -0600

    Add Counter entity, move model files

---
+++ WatchKitCoreData Framework/Data Model/Counter.swift
@@ -0,0 +1,16 @@
+//
+//  Counter.swift
+//  WatchKitCoreData
+//
+//  Created by Ben Chatelain on 5/15/15.
+//  Copyright (c) 2015 Ben Chatelain. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Counter: NSManagedObject {
+
+    @NSManaged var count: Int64
+
+}
